b62347a0349d6d2a67f16a7bcfe48175062da6cf
EnzoAliatis <enzo.aliatis@gmail.com>
2020-03-14 14:10:54 -0500

    ahora si estamos listo

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const App = () => (
+  <Title>Estamos listos para seguir</Title>
+)
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+`
